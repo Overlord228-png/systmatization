ad3bc4b14dd2a81d32b1a18c338a23bf6cc5f02b
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2023-12-24 14:35:58 +0200

    add className and import style

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "scss/_main.scss"
 
 export default function Main() {
     return(
-        <div className="">
+        <div className="main__wrapper">
             Main
         </div>
     )
