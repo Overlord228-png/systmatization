ae118ea166d0388607298432359fb1c1d83066d3
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-01-08 13:54:31 +0200

    add scss layout

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "scss/_main.scss"
+import Shower from "img/big_fotos/showreel2.png"
 
 export default function Main() {
     return(
-        <div className="main__wrapper">
-            Main
-        </div>
+        <section className="main__wrapper">
+            <div className="main__content">
+                <h1 className="text__title">
+                    Агентство мультимедийного концептуального контента и прямых трансляций для бизнеса
+                </h1>
+                <div className="logo__container">
+                    <img className="main__logo" src={Shower} alt="" />
+                </div>
+            </div>
+        </section>
     )
 };
