e70a416fce04f0686beedcd4080a2537faef5e99
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-01-08 13:53:50 +0200

    add scss layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "img/LOGO.svg"
 import "scss/_header.scss"
 
 export default function Header() {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
     return (
         <div className='header'>
             <div className="header__wrapper">
                 <a className='header_logo' href="/src/pages/Main.jsx">
                     <img src={Logo} alt="Logo" className='logo'/>
                 </a>
-                <ul className="">
-                    <li className="">Работы</li>
-                    <li className="">Услуги</li>
-                    <li className="">О нас</li>
-                    <li className="">Отзывы</li>
-                    <li className="">Blog</li>
-                    <li className="">Контакты</li>
-                </ul>
-                <button className="btn_header">Запросить стоимость</button>
+                <nav className='header__menu'>
+                    <ul className="menu__list">
+                        <li className="list__item">Работы</li>
+                        <li className="list__item">Услуги</li>
+                        <li className="list__item">О нас</li>
+                        <li className="list__item">Отзывы</li>
+                        <li className="list__item">Blog</li>
+                        <li className="list__item">Контакты</li>
+                    </ul>
+                </nav>
+                <button className="btn_header">
+                    <p className='btn-text'>
+                        Запросить стоимость
+                    </p>
+                </button>
+                <div className={`burger-menu ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
+                    <div className={`burger-icon ${isMobileMenuOpen ? 'open' : ''}`}></div>
+                </div>
             </div>
         </div>
     )
